22eab1753a0682cfe942f659638cd022d9a21590
sherwynds@gmail.com <sherwynds@gmail.com>
2020-06-09 01:13:43 -0700

    Fetches data from multiple province

--- plotter.py
+++ plotter.py
@@ -11,7 +11,25 @@ class Plotter:
 
     BC = pd.DataFrame()
 
+    data = {
+        'British Columbia': pd.DataFrame(),
+        'Alberta': pd.DataFrame(),
+        'Saskatchewan': pd.DataFrame(),
+        'Manitoba': pd.DataFrame(),
+        'Ontario': pd.DataFrame(),
+        'Quebec': pd.DataFrame(),
+        'Newfoundland and Labrador': pd.DataFrame(),
+        'Prince Edward Island': pd.DataFrame(),
+        'Nova Scotia': pd.DataFrame(),
+        'New Brunswick': pd.DataFrame(),
+        'Yukon': pd.DataFrame(),
+        'Northwest Territories': pd.DataFrame(),
+        'Nunavut': pd.DataFrame(),
+    }
+
+
     def __init__(self):
+        self.get_data()
         result = json_normalize((requests.request("GET", "https://api.covid19api.com/dayone/country/Canada/status/confirmed", headers={}, data={})).json())
         result = result.groupby(['Province'])
         self.BC = result.get_group("British Columbia")
@@ -22,6 +40,31 @@ class Plotter:
         self.BC.at[0, 'DailyCases'] = self.BC.at[0, 'Cases']
         (self.BC)['Date'] = (self.BC)['Date'].apply(lambda x: x[0:10])
         (self.BC)['Date'] = pd.to_datetime((self.BC)['Date'])
+    
+    def get_data(self):
+        cases = json_normalize((requests.request("GET", "https://api.covid19api.com/dayone/country/Canada/status/confirmed", headers={}, data={})).json())
+        cases = cases.groupby(['Province'])
+        for prov in self.data:
+            if prov not in cases.groups:
+                # TODO: handle this functionality here
+                pass
+            else:
+                df = cases.get_group(prov)
+                df = df[['Date', 'Cases']]
+                index = np.arange(0, len(df.index))
+                df.set_index(index, inplace=True)
+                df[['DailyCases']] = df[['Cases']].diff()
+                df.at[0, 'DailyCases'] = df.at[0, 'Cases']
+                df['Date'] = df['Date'].apply(lambda x: x[0:10])
+                df['Date'] = pd.to_datetime(df['Date'])
+                self.data[prov] = df
+        for prov in self.data:
+            print(prov)
+            length = len(self.data[prov].index)
+            print(self.data[prov].head(length))
+                
+
+
     
     def plot_BC_cases(self):
         p = figure(title="BC Test Plot", x_axis_type='datetime', plot_height=700, plot_width=1400)
